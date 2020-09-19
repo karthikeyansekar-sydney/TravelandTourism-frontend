c1e9d589f36daf1436dadf01427386a98c8399f5
karthikeyan sekar <karthikeyansep27@gmail.com>
2020-09-19 12:20:27 +1000

    initial logo and navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
-
+// Both BrowserRouter and HashRouter components were introduced in React Router ver. 4 as subclasses of Router class. Simply, BrowserRouter syncs the UI with the current URL in your browser, This is done by the means of HTML-5 History API. On the other hand, HashRouter uses the Hash part of your URL to sync.
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Navbar/>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+return (
+  <nav className="navbar">
+<div className="navbar-container">
+<Link to="/" className="navbar-logo">
+SKTR <i class="fas fa-umbrella-beach" />
+</Link>
+
+</div>
+  </nav>
+)
+};
+
+export default Navbar;
